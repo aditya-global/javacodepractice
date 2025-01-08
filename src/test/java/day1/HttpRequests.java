9f8bf5b84d0a91eb9aa1e5e9a9fb09216fa207ca
Aditya Gupta <aditya.gupta@vfirst.com>
2025-01-08 15:43:00 +0530

    pushing changes to new feature branch

--- src/test/java/day1/HttpRequests.java
+++ src/test/java/day1/HttpRequests.java
@@ -1,12 +1,17 @@
 package day1;
 
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*; //restassured internally uses hamcrest matchers
 
 public class HttpRequests {
 
-    @Test
+    int id;
+
+    @Test (priority = 1)
     void getUsers() {
 
         when()
@@ -15,5 +20,34 @@ public class HttpRequests {
                 .body("page", equalTo(2))
                 .log()
                 .body();
+    }
+    @Test(priority = 2)
+    void  createUser(){
+        HashMap<String,String> hm = new HashMap<>();
+        hm.put("name","Aditya");
+        hm.put("job","QA");
+
+
+        //  This process posts an api request and 'then' check the status code
+        //  and logs the api response body
+
+        given()
+                .contentType("application/json")
+                .body(hm)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .statusCode(201)
+                .log().body();
+
+        // Same post request but for capturing a response parameter
+
+        id = given()
+                .contentType("application/json")
+                .body(hm)
+                .when()
+                .post("https://reqres.in/api/users")
+                .jsonPath().getInt("id");
+
     }
 }

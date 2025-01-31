5816d9f66db6df44189568d196eb6b2fd15f83c5
Aditya Gupta <aditya.gupta@vfirst.com>
2025-01-31 12:36:49 +0530

    day 4

---
+++ src/test/java/day4/ParsingJSONResponse.java
@@ -0,0 +1,14 @@
+package day4;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class ParsingJSONResponse {
+
+    @Test
+            void testJSONResponse(){
+
+    }
+
+}
